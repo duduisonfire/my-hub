1d44b0e4fc13333342f10614308cbd64d09c33fc
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-12-29 14:43:19 -0300

    try run

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+//First Run
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app-root/app.component';
